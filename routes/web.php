8275d3ba7c7d6486cadc2d2703e9f8966baf7de9
Akeu-Andrade <akeusantana@gmail.com>
2021-10-12 12:02:39 -0300

    fix: remove commet

--- routes/web.php
+++ routes/web.php
@@ -37,39 +37,3 @@ Route::middleware(['middleware' => 'auth'])->group(function() {
 	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'App\Http\Controllers\ProfileController@password']);
 });
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-//
-//Auth::routes();
-//
-//Route::get('/home', [App\Http\Controllers\Web\Home\HomeController::class, 'index'])->name('home');
-//Auth::routes();
-//
-//Route::get('/home', 'App\Http\Controllers\Web\Home\HomeController@index')->name('home')->middleware('auth');
-//
-//Route::group(['middleware' => 'auth'], function () {
-//	Route::get('table-list', function () {
-//		return view('pages.table_list');
-//	})->name('table');
-//
-//    Route::resource('produto', ProdutoController::class);
-//    Route::resource('categoria', CategoriaController::class);
-//
-////    Route::get('categoria', 'App\Http\Controllers\Web\Produto\CategoriaController@index')->name('categoria');
-////    Route::delete('categoria/{categoria}', 'App\Http\Controllers\Web\Produto\CategoriaController@destroy')->name('categoria.destroy');
-////    Route::post('categoria/create', 'App\Http\Controllers\Web\Produto\CategoriaController@create')->name('categoria.create');
-//
-//    Route::get('notifications', function () {
-//		return view('pages.notifications');
-//	})->name('notifications');
-//
-//});
-//
-//Route::group(['middleware' => 'auth'], function () {
-//    Route::resource('user', 'App\Http\Controllers\UserController', ['except' => ['show']]);
-//	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'App\Http\Controllers\ProfileController@edit']);
-//	Route::put('profile', ['as' => 'profile.update', 'uses' => 'App\Http\Controllers\ProfileController@update']);
-//	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'App\Http\Controllers\ProfileController@password']);
-//});
-
